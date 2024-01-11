dfea6562541671283f5d65190b237802e64b5eb5
Rohan Awhad <rohanavad007@gmail.com>
2024-01-10 19:25:05 -0700

    Moved get_topics function to utils

--- main.py
+++ main.py
@@ -87,77 +87,9 @@ def main(text: str):
   plt.savefig('summary_similarity_matrix.png', dpi=300)
 
   # Run the community detection algorithm
-
-  def get_topics(title_similarity, num_topics = 8, bonus_constant = 0.25, min_size = 3):
-
-    proximity_bonus_arr = np.zeros_like(title_similarity)
-    for row in range(proximity_bonus_arr.shape[0]):
-      for col in range(proximity_bonus_arr.shape[1]):
-        if row == col:
-          proximity_bonus_arr[row, col] = 0
-        else:
-          proximity_bonus_arr[row, col] = 1/(abs(row-col)) * bonus_constant
-          
-    title_similarity += proximity_bonus_arr
-
-    title_nx_graph = nx.from_numpy_array(title_similarity)
-
-    desired_num_topics = num_topics
-    # Store the accepted partitionings
-    topics_title_accepted = []
-
-    resolution = 0.85
-    resolution_step = 0.01
-    iterations = 40
-
-    # Find the resolution that gives the desired number of topics
-    topics_title = []
-    while len(topics_title) not in [desired_num_topics, desired_num_topics + 1, desired_num_topics + 2]:
-      topics_title = community.louvain_communities(title_nx_graph, weight = 'weight', resolution = resolution)
-      resolution += resolution_step
-    topic_sizes = [len(c) for c in topics_title]
-    sizes_sd = np.std(topic_sizes)
-    modularity = community.modularity(title_nx_graph, topics_title, weight = 'weight', resolution = resolution)
-
-    lowest_sd_iteration = 0
-    # Set lowest sd to inf
-    lowest_sd = float('inf')
-
-    for i in range(iterations):
-      topics_title = community.louvain_communities(title_nx_graph, weight = 'weight', resolution = resolution)
-      modularity = community.modularity(title_nx_graph, topics_title, weight = 'weight', resolution = resolution)
-      
-      # Check SD
-      topic_sizes = [len(c) for c in topics_title]
-      sizes_sd = np.std(topic_sizes)
-      
-      topics_title_accepted.append(topics_title)
-      
-      if sizes_sd < lowest_sd and min(topic_sizes) >= min_size:
-        lowest_sd_iteration = i
-        lowest_sd = sizes_sd
-        
-    # Set the chosen partitioning to be the one with highest modularity
-    topics_title = topics_title_accepted[lowest_sd_iteration]
-    print(f'Best SD: {lowest_sd}, Best iteration: {lowest_sd_iteration}')
-    
-    topic_id_means = [sum(e)/len(e) for e in topics_title]
-    # Arrange title_topics in order of topic_id_means
-    topics_title = [list(c) for _, c in sorted(zip(topic_id_means, topics_title), key = lambda pair: pair[0])]
-    # Create an array denoting which topic each chunk belongs to
-    chunk_topics = [None] * title_similarity.shape[0]
-    for i, c in enumerate(topics_title):
-      for j in c:
-        chunk_topics[j] = i
-              
-    return {
-      'chunk_topics': chunk_topics,
-      'topics': topics_title
-      }
-
   # Set num_topics to be 1/4 of the number of chunks, or 8, which ever is smaller
   num_topics = min(int(num_1_chunks / 4), 8)
-  topics_out = get_topics(summary_similarity_matrix, num_topics = num_topics, bonus_constant = 0.2)
+  topics_out = utils.get_topics(summary_similarity_matrix, num_topics=num_topics, bonus_constant=0.2)
   chunk_topics = topics_out['chunk_topics']
   topics = topics_out['topics']
 
