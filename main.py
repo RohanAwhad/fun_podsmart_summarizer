d278a68da0aa2d0556edddd45985c2060659cf80
Rohan Awhad <rohanavad007@gmail.com>
2024-01-10 19:13:10 -0700

    Refactor code to remove unused imports and functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # [Reference]: https://towardsdatascience.com/summarize-podcast-transcripts-and-long-texts-better-with-nlp-and-ai-e04c89d3b2cb
 
 from datetime import datetime
-import pandas as pd
 import numpy as np
 import json
 import os
@@ -10,74 +9,29 @@ from scipy.spatial.distance import cosine
 import networkx as nx
 from networkx.algorithms import community
 
-# from langchain.llms.openai import OpenAI
-from langchain_community.chat_models import ChatOpenAI as OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains.llm import LLMChain
-# from langchain.embeddings import OpenAIEmbeddings
-from langchain.docstore.document import Document
-from langchain.chains.summarize import load_summarize_chain
-
 import json
-import pandas as pd
 from nltk.tokenize import sent_tokenize
 
 from src import utils
 from src.encoder_service import encode
 from src.pdf_reader import pdf_to_text
+from src.summarizer import summarize_stage_1, summarize_stage_2
 
 with open('chapter_8_biocomputing_reading.txt', 'r') as f: text = f.read()
 
-# text = pdf_to_text('./a_brief_history_of_data_viz.pdf', offset=1, limit=31)
-# with open('./podsmart_blog.json', 'r') as f:
-#   text = json.load(f)['text']
 
 
+# split into sentences
 sentences = sent_tokenize(text)
-print(sentences[:3])
 # further split by comma
 segments = [sent.split(',') for sent in sentences]
+# flaten
 segments = [x for sent in segments for x in sent]
-print(segments[:3])
 
 
 sentences = utils.create_sentences(segments, MIN_WORDS=20, MAX_WORDS=80)
 chunks = utils.create_chunks(sentences, CHUNK_LENGTH=5, STRIDE=1)
 chunks_text = [chunk['text'] for chunk in chunks]
-
-
-def summarize_stage_1(chunks_text):
-  
-  print(f'Start time: {datetime.now()}')
-
-  # Prompt to get title and summary for each chunk
-  map_prompt_template = """Firstly, give the following text an informative title. Then, write a 75-100 word summary of the following text:
-  {text}
-
-  Return your answer in the following format:
-  Title | Summary...
-  e.g. 
-  Why Artificial Intelligence is Good | AI can make humans more productive by automating many repetitive processes.
-
-  TITLE AND CONCISE SUMMARY:"""
-
-  map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
-
-  # Define the LLMs
-  map_llm = OpenAI(temperature=0, model_name = 'gpt-3.5-turbo-1106')
-  map_llm_chain = LLMChain(llm = map_llm, prompt = map_prompt)
-  map_llm_chain_input = [{'text': t} for t in chunks_text]
-  # Run the input through the LLM chain (works in parallel)
-  map_llm_chain_results = map_llm_chain.apply(map_llm_chain_input)
-
-  stage_1_outputs = utils.parse_title_summary_results([e['text'] for e in map_llm_chain_results])
-
-  print(f'Stage 1 done time {datetime.now()}')
-
-  return {
-    'stage_1_outputs': stage_1_outputs
-  }
-
 
 if not (os.path.exists('stage_1_summaries.txt') and os.path.exists('stage_1_titles.txt')):
   #raise Exception('shouldnt be here')
@@ -95,7 +49,6 @@ if not (os.path.exists('stage_1_summaries.txt') and os.path.exists('stage_1_titl
   with open('stage_1_titles.txt', 'w') as f:
     for s in stage_1_titles:
       f.write(s + '\n---\n')
-
 else:
   with open('stage_1_summaries.txt', 'r') as f:
     stage_1_summaries = f.read().split('---')[:-1]
@@ -221,94 +174,6 @@ for i in range(1, len(chunk_topics)):
 plt.savefig('chunk_topics.png', dpi=300)
 #exit(0)
 
-
-def summarize_stage_2(stage_1_outputs, topics, summary_num_words = 250):
-  print(f'Stage 2 start time {datetime.now()}')
-  
-  # Prompt that passes in all the titles of a topic, and asks for an overall title of the topic
-  title_prompt_template = """Write an informative title that summarizes each of the following groups of titles. Make sure that the titles capture as much information as possible, 
-  and are different from each other:
-  {text}
-  
-  Return your answer in a numbered list, with new line separating each title: 
-  1. Title 1
-  2. Title 2
-  3. Title 3
-
-  TITLES:
-  """
-
-  map_prompt_template = """Wite a 75-100 word summary of the following text:
-    {text}
-
-    CONCISE SUMMARY:"""
-
-  combine_prompt_template = 'Write a ' + str(summary_num_words) + """-word summary of the following, removing irrelevant information. Finish your answer:
-  {text}
-  """ + str(summary_num_words) + """-WORD SUMMARY:"""
-
-  title_prompt = PromptTemplate(template=title_prompt_template, input_variables=["text"])
-  map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
-  combine_prompt = PromptTemplate(template=combine_prompt_template, input_variables=["text"])
-
-  topics_data = []
-  for c in topics:
-    topic_data = {
-      'summaries': [stage_1_outputs[chunk_id]['summary'] for chunk_id in c],
-      'titles': [stage_1_outputs[chunk_id]['title'] for chunk_id in c]
-    }
-    topic_data['summaries_concat'] = ' '.join(topic_data['summaries'])
-    topic_data['titles_concat'] = ', '.join(topic_data['titles'])
-    topics_data.append(topic_data)
-    
-  # Get a list of each community's summaries (concatenated)
-  topics_summary_concat = [c['summaries_concat'] for c in topics_data]
-  topics_titles_concat = [c['titles_concat'] for c in topics_data]
-
-  # Concat into one long string to do the topic title creation
-  topics_titles_concat_all = ''''''
-  for i, c in enumerate(topics_titles_concat):
-    topics_titles_concat_all += f'''{i+1}. {c}
-    '''
-  
-  # print('topics_titles_concat_all', topics_titles_concat_all)
-
-  title_llm = OpenAI(temperature=0, model_name = 'gpt-3.5-turbo-1106')
-  title_llm_chain = LLMChain(llm = title_llm, prompt = title_prompt)
-  title_llm_chain_input = [{'text': topics_titles_concat_all}]
-  title_llm_chain_results = title_llm_chain.apply(title_llm_chain_input)
-  
-  
-  # Split by new line
-  titles = title_llm_chain_results[0]['text'].split('\n')
-  # Remove any empty titles
-  titles = [t for t in titles if t != '']
-  # Remove spaces at start or end of each title
-  titles = [t.strip() for t in titles]
-
-  map_llm = OpenAI(temperature=0, model_name = 'gpt-3.5-turbo-1106')
-  reduce_llm = OpenAI(temperature=0, model_name = 'gpt-3.5-turbo-1106', max_tokens = 1024)
-
-  # Run the map-reduce chain
-  docs = [Document(page_content=t) for t in topics_summary_concat]
-  chain = load_summarize_chain(chain_type="map_reduce", map_prompt = map_prompt, combine_prompt = combine_prompt, return_intermediate_steps = True,
-                              llm = map_llm, reduce_llm = reduce_llm)
-
-  output = chain({"input_documents": docs}, return_only_outputs = True)
-  summaries = output['intermediate_steps']
-  stage_2_outputs = [{'title': t, 'summary': s} for t, s in zip(titles, summaries)]
-  final_summary = output['output_text']
-
-  # Return: stage_1_outputs (title and summary), stage_2_outputs (title and summary), final_summary, chunk_allocations
-  out = {
-    'stage_2_outputs': stage_2_outputs,
-    'final_summary': final_summary
-  }
-  print(f'Stage 2 done time {datetime.now()}')
-  
-  return out
-
-
 # Query GPT-3 to get a summarized title for each topic_data
 out = summarize_stage_2(stage_1_outputs, topics, summary_num_words = 250)
 stage_2_outputs = out['stage_2_outputs']
@@ -319,8 +184,6 @@ final_summary = out['final_summary']
 
 print(stage_2_outputs)
 with open('stage_2_outputs.jsonl', 'w') as f:
-  for e in stage_2_outputs:
-    f.write(json.dumps(e) + '\n')
+  for e in stage_2_outputs: f.write(json.dumps(e) + '\n')
 
-with open('final_summary.txt', 'w') as f:
-  f.write(final_summary)
+with open('final_summary.txt', 'w') as f: f.write(final_summary)
