5c58982bd0d9fcf85533d4e53be93e6698fd4783
Rohan Awhad <rohanavad007@gmail.com>
2024-01-20 20:39:51 -0700

    Add Dockerfile and lambda_function.py

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM public.ecr.aws/lambda/python:3.11
+
+COPY requirements.txt ./
+
+RUN pip install -r requirements.txt
+
+COPY src ${LAMBDA_TASK_ROOT}
+COPY main.py ${LAMBDA_TASK_ROOT}
+COPY lambda_function.py ${LAMBDA_TASK_ROOT}
+
+CMD [ "lambda_function.handler" ]
---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+from main import main as run
+from pydantic import BaseModel
+from typing import Optional, List, Dict, Any
+
+# read from sqs into messages_list
+# loop over messages_list
+#   get url from body
+#   get receipt_handle from message
+#   run the main function
+#   update item with url =`url` with data to dynamodb
+#   delete message from sqs with receipt_handle
+
+class LambdaOut(BaseModel):
+  stages: List[Dict[str, Any]]
+  final_summary: Optional[str] = None
+  markdown_summary: str
+  status: int = 200
+  error: Optional[str] = None
+
+def handler(event, context):
+  text = event['text'].strip()
+  try:
+    out = run(text)
+    stages = [out.stage_1_outputs, out.stage_2_outputs]
+    stages[0]['chunk_topics'] = out.chunk_topics
+    return LambdaOut(
+      stages=stages,
+      final_summary=out.final_summary,
+      markdown_summary=out.markdown_summary
+    ).model_dump_json()
+  except Exception as e:
+    return LambdaOut(
+      stages=[],
+      markdown_summary='',
+      status=500,
+      error=str(e)
+    ).model_dump_json()
