620a884b7fde5cbb78faf866590a4f9643ce3bbf
Rohan Awhad <rohanavad007@gmail.com>
2024-01-28 18:06:04 -0700

    updated soln arch and as a result lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,7 @@
+import boto3
+import json
+import os
+
 from main import main as run
 from pydantic import BaseModel
 from typing import Optional, List, Dict, Any
@@ -9,22 +13,52 @@ class LambdaOut(BaseModel):
   status: int = 200
   error: Optional[str] = None
 
+  # input data passing on
+  url: str
+  title: str
+  text: str
+
+sqs = boto3.client('sqs')
+inp_sqs_url = os.environ['INP_SQS_URL']
+
+sns = boto3.client('sns')
+out_sns_topic_arn = os.environ['OUT_SNS_TOPIC_ARN']
+
 def handler(event, context):
-  text = event['text'].strip()
-  try:
-    out = run(text)
-    stages = [out.stage_1_outputs, out.stage_2_outputs]
-    stages = [{'outputs': s} for s in stages]
-    stages[0]['chunk_topics'] = out.chunk_topics
-    return LambdaOut(
-      stages=stages,
-      final_summary=out.final_summary,
-      markdown_summary=out.markdown_summary
-    ).model_dump_json()
-  except Exception as e:
-    return LambdaOut(
-      stages=[],
-      markdown_summary='',
-      status=500,
-      error=str(e)
-    ).model_dump_json()
+  if not event: return
+  batch_item_failures = []
+
+  for message in event['Records']:
+    body = json.loads(message['body'])
+    inp_data = json.loads(body['Message'])
+    text = inp_data['text'].strip()
+    receipt_handle = message['receiptHandle']
+    try:
+      out = run(text)
+      stages = [out.stage_1_outputs, out.stage_2_outputs]
+      stages = [{'outputs': s} for s in stages]
+      stages[0]['chunk_topics'] = out.chunk_topics
+
+      # write to an sns topic
+      sns.publish(
+        TopicArn=out_sns_topic_arn,
+        Message=LambdaOut(
+          stages=stages,
+          final_summary=out.final_summary,
+          markdown_summary=out.markdown_summary,
+          url=inp_data['url'],
+          title=inp_data['title'],
+          text=inp_data['text']
+        ).model_dump_json()
+      )
+
+      # delete message from sqs
+      sqs.delete_message(
+        QueueUrl=inp_sqs_url,
+        ReceiptHandle=receipt_handle
+      )
+    except Exception as e:
+      print(e)
+      batch_item_failures.append({'itemIdentifier':message['messageId']})
+
+  return {'batchItemFailures': batch_item_failures}
