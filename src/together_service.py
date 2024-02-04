3819358571b6e0c356e7411ed7c35fbf6122ad19
Rohan Awhad <rohanavad007@gmail.com>
2024-02-04 01:21:17 -0700

    updated retry logic

--- src/together_service.py
+++ src/together_service.py
@@ -45,9 +45,11 @@ class _LLM:
     self.top_k = top_k
     self.stop_tokens = ['</s>', '[/INST]'] + stop_tokens if stop_tokens else ['</s>', '[/INST]']
 
+    self.max_retries = 3
+
   async def _generate_batch(self) -> np.ndarray:
     outputs = []
-    for txt in self.text_list: await self._todo.put((txt))
+    for txt in self.text_list: await self._todo.put((txt, 0))
     
     workers = [asyncio.create_task(self.worker()) for _ in range(self.num_workers)]
     await self._todo.join()
@@ -62,33 +64,51 @@ class _LLM:
       except asyncio.CancelledError: return
 
   async def process_one(self):
-    prompt = await self._todo.get()
-    try:
-      data = {
-          "model": self.llm,
-          "prompt": prompt,
-          "max_tokens": self.max_tokens,
-          "stop": self.stop_tokens,
-          "temperature": self.temperature,
-          "top_p": self.top_p,
-          "top_k": self.top_k,
-          "repetition_penalty": 1,
-          "n": 1
-      }
-      headers = {
-          "accept": "application/json",
-          "content-type": "application/json",
-          "Authorization": "Bearer " + api_key
-      }
-      async with self.session.post(self.url, json=data, headers=headers) as response:
-        if response.status != 200: raise Exception(f"Failed to generate text: {prompt}. Got text: {await response.text()}")
-        self.responses.append(await response.json())
-    except Exception as e:
-      logger.error(e)
-      await self._todo.put(prompt)
-    finally:
-      self.pbar.update(1)
-      self._todo.task_done()
+    prompt, _ = await self._todo.get()
+    res = None
+    backoff = 2
+    interval = 0.5
+    for attempt in range(self.max_retries):
+      try:
+        data = {
+            "model": self.llm,
+            "prompt": prompt,
+            "max_tokens": self.max_tokens,
+            "stop": self.stop_tokens,
+            "temperature": self.temperature,
+            "top_p": self.top_p,
+            "top_k": self.top_k,
+            "repetition_penalty": 1,
+            "n": 1
+        }
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "Authorization": "Bearer " + api_key
+        }
+
+        async with self.session.post(self.url, json=data, headers=headers) as response:
+          print('response', response)
+          if response.status == 200:
+            res = await response.json()
+          raised_exc = None
+      #except Exception as e:
+        backoff_interval = interval * (backoff ** attempt)
+      except aiohttp.ClientError as e:
+        raise_exc = e
+        logger.error(e)
+        #if retry_attempts < self.max_retries:
+        #  logger.warning(f"Retrying prompt ...")
+        #  await self._todo.put((prompt, retry_attempts + 1))
+
+      await asyncio.sleep(backoff_interval)
+
+    self.pbar.update(1)
+    self._todo.task_done()
+    if res is None:
+      raise Exception(f"Failed to generate text: {prompt}. Got text: {await response.text()}")
+    else:
+      self.responses.append(res)
         
 
 async def _generate(
@@ -100,7 +120,9 @@ async def _generate(
   top_k: int=50,
   stop_tokens: Optional[List[str]]=None,
 ) -> List[str]:
-  async with aiohttp.ClientSession() as session:
+
+  timeout = aiohttp.ClientTimeout(total=10)
+  async with aiohttp.ClientSession(timeout=timeout) as session:
     _llm = _LLM(
       url,
       llm,
@@ -136,5 +158,6 @@ def generate(
 
 if __name__ == '__main__':
   # testing service
-  print(generate(['<s>[INST] What is the capital of France? [/INST]']))
+  llm = 'something'
+  print(generate(['<s>[INST] What is the capital of France? [/INST]'], llm))
   exit()
